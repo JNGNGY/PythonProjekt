34a9f8af00ef2e54391cc527097f1d1750bd0633
MyNameIsHappy <65649945+MyNameIsHappy@users.noreply.github.com>
2020-12-21 14:53:53 +0100

    Update Aufgabe1.py
    test kroll marco

--- Aufgabe1.py
+++ Aufgabe1.py
@@ -1,1 +1,10 @@
 #Aufgabe1
+
+def fib(a=0, b=1):
+    """Generator that yields Fibonacci numbers. `a` and `b` are the seed values"""
+    while True:
+        yield a
+        a, b = b, a + b
+
+f = fib()
+print(', '.join(str(next(f)) for _ in range(25)))
