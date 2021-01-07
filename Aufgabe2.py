57eaf6751417ad2f9aca8f09637aafd1d710eb4a
taangja1 <jan.nguyen@swisscom.com>
2021-01-07 13:45:12 +0100

    Aufgabe2 Done

--- Aufgabe2.py
+++ Aufgabe2.py
@@ -1,2 +1,8 @@
-#Aufgabe2
-print("hell")
+# Python 3.8. 3.9
+# Mac OSx, Windows 10
+#Creator: Nguyen Jan, Kroll Marco, Kayis Boran
+import csv
+with open('File_C_Inf2019l_Baba.csv', encoding='mac_roman', newline='') as csvfile:
+ data = csv.DictReader(csvfile, delimiter=';')
+ for row in data:
+   print(row['Vorname'], row['Name'],',',row['Strasse'], row['Hausnr.'],',',row['PLZ'], row['Ort'],',','+41', row['Telefon 1'][1:])
