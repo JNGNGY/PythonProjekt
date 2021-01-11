47825df269ed20298f529a1e69801b87ecca4f3e
taangja1 <jan.nguyen@swisscom.com>
2021-01-11 16:48:45 +0100

    fertig mit däm scheiss

--- Aufgabe1.py
+++ Aufgabe1.py
@@ -3,19 +3,45 @@
 #Creator: Nguyen Jan, Kroll Marco, Kayis Boran
 import csv
 from os import write
-with open('File_B_In2019_L.csv', encoding='mac_roman') as csvdatei, open('File_A_Inf2019_L.csv', encoding='mac_roman') as csvdatei1 :           # öffnet csv file
+with open('File_B_In2019_L.csv', encoding='mac_roman') as csvdatei, open('File_A_Inf2019_L.csv', encoding='mac_roman') as csvdatei1:           # öffnet csv file
     csv_reader = csv.DictReader(csvdatei, delimiter=';')
     csv_reader1 = csv.DictReader(csvdatei1, delimiter=';')                                                                                      # liest Datein ein mit Header
-    csvfile = open('File_C_Inf2019l_Baba.csv', 'w', encoding='mac_roman',  newline='')                                                          # erstellt neue CSV File                
+    csvfile = open('1.csv', 'w', encoding='mac_roman',  newline='')                                                          # erstellt neue CSV File                
     myFields = ['ID', 'Vorname', 'Name', 'Strasse', 'Hausnr.', 'PLZ', 'Ort', 'Telefon 1']                                                       # Variable für Header
     writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=myFields)                                                                        # Schreibt die Datein ein mit Header        
     writer.writeheader()                                                                                                                        # Schreibt den Header    
-    for row in csv_reader:                                                                                                                      # Für jede Zeile in der Variable csv_reader
+    for row in csv_reader:
+        del row ['ID']
         writer.writerow(row)
-    for row in csv_reader1:                                                                                                                     # Für jede Zeile in der Variable csv_reader
-        writer.writerow(row)                                                                                                                    # Schreibt die Datein ein            
-    csvfile.close()
+    for row in csv_reader1:
+        del row ['ID']                                                                                                                     # Für jede Zeile in der Variable csv_reader
+        writer.writerow(row)    
 
+with open('1.csv', encoding='mac_roman') as in_file, open('2.csv','w') as out_file:
+    seen = set() # set for fast O(1) amortized lookup
+    for line in in_file:
+        if line not in seen: 
+            seen.add(line)
+            out_file.write(line)
+
+import csv
+from os import write
+with open('2.csv', encoding='mac_roman') as test:
+    csvreader = csv.DictReader(test, delimiter=';')
+    csvfile = open('File_C_Inf2019l_Baba.csv', 'w', encoding='mac_roman',  newline='') 
+    myFields = ['ID', 'Vorname', 'Name', 'Strasse', 'Hausnr.', 'PLZ', 'Ort', 'Telefon 1']
+    writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=myFields)                                                                        # Schreibt die Datein ein mit Header        
+    writer.writeheader()
+    for count, row in enumerate(csvreader,1):
+        row['ID'] = count
+        writer.writerow(row)
+                
+    
+                    
+
+import os
+os.remove('1.csv')
+os.remove('2.csv')
 
  #-----------------------------------------------History--------------------------------------------------------------------------------   
     
