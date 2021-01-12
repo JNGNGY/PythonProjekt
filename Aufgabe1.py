ea19088bdf8ae47b02133437c4245862c4745996
taangja1 <jan.nguyen@swisscom.com>
2021-01-12 10:39:44 +0100

    lol

--- Aufgabe1.py
+++ Aufgabe1.py
@@ -3,36 +3,34 @@
 #Creator: Nguyen Jan, Kroll Marco, Kayis Boran
 import csv
 from os import write
-with open('File_B_In2019_L.csv', encoding='mac_roman') as csvdatei, open('File_A_Inf2019_L.csv', encoding='mac_roman') as csvdatei1:           # öffnet csv file
-    csv_reader = csv.DictReader(csvdatei, delimiter=';')
-    csv_reader1 = csv.DictReader(csvdatei1, delimiter=';')                                                                                      # liest Datein ein mit Header
-    csvfile = open('1.csv', 'w', encoding='mac_roman',  newline='')                                                          # erstellt neue CSV File                
+with open('File_B_In2019_L.csv', encoding='mac_roman') as FileB, open('File_A_Inf2019_L.csv', encoding='mac_roman') as FileA:           # öffnet csv file
+    FileB_reader = csv.DictReader(FileB, delimiter=';')
+    FileA_reader = csv.DictReader(FileA, delimiter=';')                                                                                      # liest Datein ein mit Header
+    FileC = open('DatenSchreiben.csv', 'w', encoding='mac_roman',  newline='')                                                                             # erstellt neue CSV File                
     myFields = ['ID', 'Vorname', 'Name', 'Strasse', 'Hausnr.', 'PLZ', 'Ort', 'Telefon 1']                                                       # Variable für Header
-    writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=myFields)                                                                        # Schreibt die Datein ein mit Header        
+    writer = csv.DictWriter(FileC, delimiter=';', fieldnames=myFields)                                                                        # Schreibt die Datein ein mit Header        
     writer.writeheader()                                                                                                                        # Schreibt den Header    
-    for row in csv_reader:
+    for row in FileB_reader:
         del row ['ID']
         writer.writerow(row)
-    for row in csv_reader1:
+    for row in FileA_reader:
         del row ['ID']                                                                                                                     # Für jede Zeile in der Variable csv_reader
         writer.writerow(row)    
 
-with open('1.csv', encoding='mac_roman') as in_file, open('2.csv','w') as out_file:
-    seen = set() # set for fast O(1) amortized lookup
-    for line in in_file:
-        if line not in seen: 
-            seen.add(line)
-            out_file.write(line)
+with open('DatenSchreiben.csv', encoding='mac_roman') as FileWrite, open('DoppelteDatenLöschen.csv','w') as FileDeleteDuplicate:
+    duplicate = set()                                                                                                                            # set for fast O(1) amortized lookup
+    for line in FileWrite:
+        if line not in duplicate: 
+            duplicate.add(line)
+            FileDeleteDuplicate.write(line)
 
-import csv
-from os import write
-with open('2.csv', encoding='mac_roman') as test:
-    csvreader = csv.DictReader(test, delimiter=';')
-    csvfile = open('File_C_Inf2019l_Baba.csv', 'w', encoding='mac_roman',  newline='') 
+with open('DoppelteDatenLöschen.csv', encoding='mac_roman') as FileC:
+    FileC_reader = csv.DictReader(FileC, delimiter=';')
+    FileC = open('File_C_Inf2019l_Baba.csv', 'w', encoding='mac_roman',  newline='') 
     myFields = ['ID', 'Vorname', 'Name', 'Strasse', 'Hausnr.', 'PLZ', 'Ort', 'Telefon 1']
-    writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=myFields)                                                                        # Schreibt die Datein ein mit Header        
+    writer = csv.DictWriter(FileC, delimiter=';', fieldnames=myFields)                                                                        # Schreibt die Datein ein mit Header        
     writer.writeheader()
-    for count, row in enumerate(csvreader,1):
+    for count, row in enumerate(FileC_reader,1):
         row['ID'] = count
         writer.writerow(row)
                 
@@ -40,8 +38,8 @@ with open('2.csv', encoding='mac_roman') as test:
                     
 
 import os
-os.remove('1.csv')
-os.remove('2.csv')
+os.remove('DatenSchreiben.csv')
+os.remove('DoppelteDatenLöschen.csv')
 
  #-----------------------------------------------History--------------------------------------------------------------------------------   
     
--- TestFile.py
+++ TestFile.py
@@ -1,7 +1,80 @@
+import sys
 import csv
-with open('File_B_In2019_L.csv', newline='') as csvfile:
- data = csv.DictReader(csvfile, delimiter=';')
- print("ID Department Name")
- print("---------------------------------")
- for row in data:
-   print(row['Vorname'], row['Name'])
+
+#variablen definieren
+
+File_A_Inf2019l = open('File_A_Inf2019_L.csv', encoding='mac_roman')       #argumente entgegen nehmen
+File_B_Inf2019l = open('File_B_In2019_L.csv', encoding='mac_roman')
+
+#with open('File_B_In2019_L.csv', encoding='mac_roman') as File_B_Inf2019l, open('File_A_Inf2019_L.csv', encoding='mac_roman') as File_A_Inf2019l:
+
+inhaltA = File_A_Inf2019l.read()  # inhalt csv auslesen und speichern
+inhaltB = File_B_Inf2019l.read()
+  
+inhaltC = inhaltA + inhaltB     #inhalt zusammenführen
+
+zeilenC = inhaltC.split('\n')
+anzahlZeilenC, anzahlSpaltenC = len(zeilenC), 8  #grösse definieren
+
+arrC = [[0 for x in range(anzahlSpaltenC)] for y in range(anzahlZeilenC)]  # array definieren
+
+
+
+def füllenArray():
+    for i in range(anzahlZeilenC):  #array füllen
+        spalten = zeilenC[i].split(';')
+        if len(spalten) == 8:
+            for x in range(anzahlSpaltenC):
+                arrC[i][x] = spalten[x]
+                
+
+
+
+
+def erkennenDoppelteDatensätze():       #erkennt doppelte Datensätze
+    for y in range(anzahlZeilenC): #nimmt erste row
+
+            for z in range(anzahlZeilenC): #nimmt zweite row
+
+                if y != z:  #stellt sicher, dass index unterschiedlich ist
+
+                    for s in range(anzahlSpaltenC - 1):  # vergleicht Inhalt von beiden rows, -1 damit id nicht geprüft wird
+                        if s != 7 and y < anzahlZeilenC and z < anzahlZeilenC:  #verhindert index fehler
+                            if arrC[y][s + 1] == arrC[z][s + 1]:  #+1 damit id nicht geprüft wird
+                                if s == 6:  #wenn letzter wert positiv geprüft wird, doppelter Datensatz löschen
+                                    löschenDatensatz(z)
+                            else:
+                                break
+
+
+def löschenDatensatz(ds):
+    global anzahlZeilenC
+    anzahlZeilenC -= 1
+    arrC.pop(ds)     #datensatz löschen
+
+
+
+def schreibenDatei():
+    indexZähler = 0
+    print(arrC)
+    csv.register_dialect("semik.", delimiter=";")       #trenner definieren
+    datei = open('File_C_Inf2019l_VAN.csv', 'w', encoding='mac_roman')
+
+    with datei:
+        schreiber = csv.writer(datei, dialect="semik.", lineterminator='\n', delimiter=";")    #schreiber erstellen
+
+        for zeile in arrC:       #jede Zeile aus array in for schleife schreiben
+            if zeile != [0, 0, 0, 0, 0, 0, 0, 0] and zeile != ['', '', '', '', '', '', '', '']:  # verhindert schreiben von leeren zeilen
+
+                if indexZähler != 0:            #damit Header 'ID' gleich bleibt
+                    zeile[0] = indexZähler      #anpassung ID-Wert
+                schreiber.writerow(zeile)
+                indexZähler += 1
+
+#Funktionen ausführen
+
+füllenArray()
+
+erkennenDoppelteDatensätze()
+
+schreibenDatei()
